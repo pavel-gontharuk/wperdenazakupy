140f9df92ce107811dcfa90ea9ec7dda889c3c59
Pavel <pavlusius@gmail.com>
2023-09-11 02:28:49 +0200

    WIP

--- data/src/main/java/com/gontharuk/wperdenazakupy/data/database/DatabaseModule.kt
+++ data/src/main/java/com/gontharuk/wperdenazakupy/data/database/DatabaseModule.kt
@@ -2,6 +2,9 @@ package com.gontharuk.wperdenazakupy.data.database
 
 import android.content.Context
 import androidx.room.Room
+import com.gontharuk.wperdenazakupy.data.database.product.ProductDao
+import com.gontharuk.wperdenazakupy.data.database.product.ProductRepositoryImpl
+import com.gontharuk.wperdenazakupy.domain.repository.ProductRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -22,4 +25,14 @@ object DatabaseModule {
         WperdeDatabase::class.java,
         "wperde_database",
     ).build()
+
+    @Provides
+    fun provideProductDao(
+        database: WperdeDatabase
+    ): ProductDao = database.productDao()
+
+    @Provides
+    fun provideProductRepository(
+        dao: ProductDao
+    ): ProductRepository = ProductRepositoryImpl(dao)
 }
--- presentation/src/main/java/com/gontharuk/wperdenazakupy/presentation/di/product/ProductModule.kt
+++ presentation/src/main/java/com/gontharuk/wperdenazakupy/presentation/di/product/ProductModule.kt
@@ -1,7 +1,5 @@
-package com.gontharuk.wperdenazakupy.di.product
+package com.gontharuk.wperdenazakupy.presentation.di.product
 
-import com.gontharuk.wperdenazakupy.data.database.product.ProductDao
-import com.gontharuk.wperdenazakupy.data.database.product.ProductRepositoryImpl
 import com.gontharuk.wperdenazakupy.domain.repository.ProductRepository
 import com.gontharuk.wperdenazakupy.domain.usecase.productusecase.ProductUseCase
 import com.gontharuk.wperdenazakupy.domain.usecase.productusecase.ProductUseCaseImpl
@@ -13,11 +11,6 @@ import dagger.hilt.components.SingletonComponent
 @Module
 @InstallIn(SingletonComponent::class)
 object ProductModule {
-
-    @Provides
-    fun provideProductRepository(
-        dao: ProductDao
-    ): ProductRepository = ProductRepositoryImpl(dao)
 
     @Provides
     fun provideProductUseCase(
